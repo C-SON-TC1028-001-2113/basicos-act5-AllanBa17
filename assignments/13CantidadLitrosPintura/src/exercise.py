2f4e3cf221e43ef46488c8d2dc1de07ececa9e5c
AllanBa17 <a01254006@itesm.mx>
2021-08-17 20:31:51 +0000

    15Escaleras

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,13 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    area = float(input("Area a pintar en metros: "))
+    Rendimiento = int(input('Rendimiento (m2/l): '))
+    comprar = area/Rendimiento
+    print("Litros a comprar: "+str(math.ceil(comprar)))
+
+
+    
 
 
 if __name__ == '__main__':
--- assignments/15Escalera/src/exercise.py
+++ assignments/15Escalera/src/exercise.py
@@ -2,5 +2,10 @@ import math
 def main():
     #escribe tu código abajo de esta línea
 
+    Altura = int(input('Altura de la casa: '))
+    angulo = int(input('Angulo en grados: '))
+    radianes = angulo*3.14/180
+    Largo = round(Altura/math.sin(radianes))
+    print("Largo de la escalera: "+str(Largo))
 if __name__ == '__main__':
     main()
